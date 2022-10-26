c8517783efb81022116ee669c99e009e39273273
adfwer <ba123698745@126.com>
2022-10-26 21:32:03 +0800

    feat: catch mouse interrupt

--- cpu/isr.asm
+++ cpu/isr.asm
@@ -1,13 +1,12 @@
 
 set_idt_gate:
     ; param n in eax, handler address in ebx
-    
-    mov [idt_gate + 264], bx
+    mov [idt_gate + eax], bx
     shr ebx, 16
-    mov [idt_gate + 264 + 6], bx
-    mov [idt_gate + 264 + 2], word 0x08
-    mov [idt_gate + 264 + 4], byte 0
-    mov [idt_gate + 264 + 5], byte 0x8e
+    mov [idt_gate + eax + 6], bx
+    mov [idt_gate + eax + 2], word 0x08
+    mov [idt_gate + eax + 4], byte 0
+    mov [idt_gate + eax + 5], byte 0x8e
 
     ret
 
@@ -37,19 +36,44 @@ isr_install:
     out 0xa1, al
 
     ; OCW 1
-    mov al, 11111101b
+    mov al, 00000001b
     out 0x21, al
 
+    ; OCW 2
+    mov  al, 0	
+    out  0xa1, al		
+
     ; install keyboard interrupt:  IRQ1
-    mov eax, 33
+    mov eax, 33 * 8
     mov ebx, irq1
+    call set_idt_gate
+
+    ; install keyboard interrupt:  IRQ12
+    mov eax, 44 * 8
+    mov ebx, irq12
     call set_idt_gate
 
     mov [idt_register], word 4095 ; 256 * 16 - 1
     
     mov [idt_register + 2], dword idt_gate
     lidt [idt_register]
+
+    call kbc_ready
+    mov al, 0x60
+    out 0x64, al
+
+    call kbc_ready
+    mov al, 0x47
+    out 0x60, al
     
+    call kbc_ready
+    mov al, 0xd4
+    out 0x64, al
+    
+    call kbc_ready
+    mov al, 0xf4
+    out 0x60, al
+
     ret
 
 irq1:
@@ -158,6 +182,29 @@ irq1:
     out 0x20, al
 
     release:
+    iret
+
+kbc_ready:
+    kbc_ready_loop:
+        in al, 0x64 ;sta
+        and al, byte 0x02
+        cmp al, 0
+        je kbc_ready_ret
+        jmp kbc_ready_loop
+    kbc_ready_ret:
+    ret
+
+irq12:
+    cli
+
+    mov al, 0x20
+    out 0x20, al
+    mov al, 0x20
+    out 0xa0, al
+
+    in al, 0x60
+
+    call print_right
     iret
 
 irq_common_stub:
